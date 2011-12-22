04024b6aecd2ffa57426f712698890f821e98755
Slux <slux83@gmail.com>
2011-12-22 11:06:17 +0100

    started development of serializable framework
    Signed-off-by: Slux <slux83@gmail.com>

---
+++ test/cppunit/sserializable_test.cpp
@@ -0,0 +1,8 @@
+/******************************************************************************
+[FILE_HEADER_BEGIN]
+[FILE_HEADER_END]
+******************************************************************************/
+
+#include "sserializable_test.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SSerializableTest);
